09c1563af76dddf9d1fde082d102dc30f42d1c8a
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-08 20:46:37 +0900

    Create gilbutReact3.1_클래스형 컴포넌트.js

---
+++ theAteOfReact/hello-react/gilbutReact-3-컴포넌트/gilbutReact3.1_클래스형 컴포넌트.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    const name = "react";
+    return <div className="react">{name}</div>;
+  }
+}
+
+export default App;
