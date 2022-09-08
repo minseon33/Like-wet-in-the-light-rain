3a172a636c6ee3effeeb63b824923b8ccc689394
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-08 20:18:34 +0900

    Create gilbutReact2.4.8_혼자 열고닫는 태그.js

---
+++ theAteOfReact/hello-react/gilbutReact-2-JSX/gilbutReact2.4.8_혼자 열고닫는 태그.js
@@ -0,0 +1,19 @@
+{/*혼자열고 닫는 태그 selp-closing*/}
+
+import './App.css';
+
+
+function App(){
+  const name = "리액트";
+  return (
+    <>
+    <div className="react">
+      {name}
+    </div>
+    <input/>
+    </>
+    
+  );
+}
+
+export default App;
