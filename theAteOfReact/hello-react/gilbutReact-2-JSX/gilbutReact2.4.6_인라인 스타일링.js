4ae0784d2e79fdf30e3350eaac9dbc2fb6a81df0
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-08 20:18:19 +0900

    Create gilbutReact2.4.6_인라인 스타일링.js

---
+++ theAteOfReact/hello-react/gilbutReact-2-JSX/gilbutReact2.4.6_인라인 스타일링.js
@@ -0,0 +1,18 @@
+function App(){
+    const name = "리액트";
+    const style = {
+      backgroundColor : 'black',
+      color:'aqua',
+      fontSize:'48px',
+      fontWeight:'bold',
+      padding:16
+    }
+  
+    return (
+      <div style = {style}>
+        {name}
+      </div>
+    );
+  }
+  
+  export default App;
