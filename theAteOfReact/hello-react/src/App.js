e5cf1ee43fff06a698ff7bac12e1f00174275102
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-09 12:38:03 +0900

    Update App.js

--- theAteOfReact/hello-react/src/App.js
+++ theAteOfReact/hello-react/src/App.js
@@ -1,6 +1,10 @@
 import MyComponent from "./MyComponent";
 const App = () => {
-  return <MyComponent name="React">리액트</MyComponent>;
+  return (
+    <MyComponent name="React" favoriteNumber={3}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
