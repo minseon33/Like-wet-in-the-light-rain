612e84634d37a0d557aeba463777eb960da8ac3e
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-09 14:02:55 +0900

    Create gilbutReact3.4.1.2_ state를 constructor에서 꺼내기 컴포넌트.js

---
+++ theAteOfReact/hello-react/gilbutReact-3-컴포넌트/gilbutReact3.4.1.2_ state를 constructor에서 꺼내기 컴포넌트.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    number: 0,
+    fixedNumber: 0,
+  };
+  render() {
+    const { number, fixedNumber } = this.state;
+    return (
+      <div>
+        <h1>현재 숫자는 : {number}</h1>
+        <h2>바뀌지 않는 숫자 : {fixedNumber}</h2>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
