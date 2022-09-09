8ba0dbc1fec9dc4749ed57e94a49ed92689b66cd
minseon33 <109520441+minseon33@users.noreply.github.com>
2022-09-09 11:48:40 +0900

    Update MyComponent.js

--- theAteOfReact/hello-react/src/MyComponent.js
+++ theAteOfReact/hello-react/src/MyComponent.js
@@ -1,7 +1,18 @@
-const MyComponent = (props) => {
-  return <div>안녕하세요 제 이름은 {props.name} 입니다.</div>;
+import PropTypes from "prop-types";
+
+const MyComponent = ({ name, children }) => {
+  return (
+    <div>
+      안녕하세요 제 이름은 {name} 입니다.
+      <br />
+      children 값은 {children}입니다.
+    </div>
+  );
 };
 MyComponent.defaultProps = {
   name: "기본이름",
+};
+MyComponent.propTypes = {
+  name: PropTypes.string,
 };
 export default MyComponent;
